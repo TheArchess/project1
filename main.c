d5e59a9489312fc5636bd37fbe7c206c2c2d8b1b
SebastianOsorioArango <criptodesc09@gmail.com>
2022-07-27 15:32:56 -0500

    Revert "Add a new print" no me gusto
    This reverts commit 555ace9160c5edd60b78f08054f9e7e20e92d248.

--- main.c
+++ main.c
@@ -1,11 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("La vida es bella\n");
-    return(EXIT_SUCCESS);
-
-    printf("El aguacate se comio un banano\n");
-    return(EXIT_SUCCESS);
-}
+ññññ á ññññ[[{{{{#}}}}]]
