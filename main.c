555ace9160c5edd60b78f08054f9e7e20e92d248
SebastianOsorioArango <criptodesc09@gmail.com>
2022-07-27 14:55:23 -0500

    Add a new print

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-ññññ á ññññ[[{{{{#}}}}]]
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("La vida es bella\n");
+    return(EXIT_SUCCESS);
+
+    printf("El aguacate se comio un banano\n");
+    return(EXIT_SUCCESS);
+}
